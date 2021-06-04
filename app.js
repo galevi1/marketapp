7816c8c63add56b00d97407f181c98e27d087ff5
gal levi <gallevi951@gmail.com>
2021-06-04 08:40:50 +0300

    28_05_21_3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const csrf = require('csurf');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const { request } = require('express');
+const { request, response } = require('express');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
@@ -33,8 +33,21 @@ app.use(multer({ storage: fileStorage, limits: {fieldSize: 25033697} }).array('i
 app.use(express.static(path.join(__dirname, 'public'))); //איזה תיקייה היא התיקייה הציבורית שפתוחה לתמונות ולמה שצריף
 app.use('/images', express.static('images'));
 
+
 const indexController = require('./controllers/index');
+const { runInNewContext } = require('vm');
 app.use('/', indexController);
+
+//1 תיצור לי איזשהי הגנה
+const csurfProtection = csrf();
+//2 תגני כל הזמן
+app.use(csurfProtection);
+//3 מסבירה לו איך להשתמש 
+app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+});
+
 
 const port = 6060;
 app.listen(port, function(){
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,44 @@
-const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name:'dp2f6qmk7',
+    api_key:'633459321666152',
+    api_secret:'xEh14w36X-CvgcZLCCIbr6ExLt4'
+});
 
 router.get('/' , (request, response) => {
     response.render('index', {
 
     });
 })
+
+router.post('/uploadfile', async(request, response) => {
+    let image ='';
+    const files = request.files;
+
+    if(files.length > 0){
+        files.forEach(async img =>{
+            const image_name = generateImageName(100000000, 999999999);
+            const mime = img.mimetype.split('/')[0];
+            let body = {
+                resource_type:mime,
+                public_id:'myImages/' + image_name
+            }
+            await cloudinary.v2.uploader.upload(
+                img.path, body, 
+                async function(error, result){
+                    image = await result.secure_url;
+                }
+            );
+        console.log(image);
+        });
+    }
+    response.redirect('/');
+    })
+
+function generateImageName(min, max){
+    return Math.floor((Math.random() * max) + min);
+}
 
 module.exports = router;
